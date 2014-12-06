a9ae87097b698f1c59fbd1530aaa1b59bf7641cd
Hanna Ai <hangha1812@gmail.com>
2014-12-05 23:24:22 -0500

    update repo with all Aiport App files

---
+++ AiportHelper/Assets/CS4590 Final Project/Scripts/toiletFlush.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toiletFlush : MonoBehaviour {
+	
+	public AudioSource toilet;
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("toilet triggered");
+		toilet.Play();	
+	}
+	void OnTriggerExit(Collider other){
+		toilet.Stop ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-AirportHelper
-=============
-
-This is a prototype of Airport Helper application, which is a Google Glass application that assists airport travelers to navigate around the airport easier. This prototype is created with UNITY 5.0 BETA 14 
